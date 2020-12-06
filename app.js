83354c0d829df92ac632c11bc1b9e31d12ef9748
Carlos Rodriguez <karz09@gmail.com>
2020-12-05 20:46:14 -0400

    Add Enviroment Vars

---
+++ app.js
@@ -0,0 +1,110 @@
+//jshint esversion:6
+require('dotenv').config();
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const express = require("express");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+//----------------------------------------------------
+//----------------------------------------------------
+//----------------------------------------------------
+const app = express();
+
+console.log(process.env.API_KEY);
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+// ---------------------------- IGNORE FAVICON FUNCTION --------------------------------
+
+function ignoreFavicon(req, res, next) {
+  if (req.originalUrl === '/favicon.ico') {
+    res.status(204).json({nope: true});
+  } else {
+    next();
+  }
+}
+
+app.use(ignoreFavicon);
+// ---------------------------- IGNORE FAVICON FUNCTION --------------------------------
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect("mongodb://localhost:27017/userDB");
+//----------------------------------------------------
+//----------------------------------------------------
+//----------------------------------------------------
+
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
+
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function(err){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                }
+            }
+        }
+    })
+});
+
+
+
+let port = process.env.PORT;
+if (port == null || port =="") {
+    port = 3000;
+}
+
+app.listen(port, function(){
+    console.log("Server has started successfully");
+})
